819c1dd95ded4a3685e7f0924f998a1b52f5de73
Akila Lakshitha <136437833+mrakiyaaa@users.noreply.github.com>
2025-03-31 23:47:24 +0530

    feat(hoemPage): home page UI done

---
+++ lib/components/search_field.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:walkwise/constants/colors.dart';
+
+class SearchField extends StatelessWidget {
+  const SearchField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.search, color: Colors.grey),
+        hintText: "Find Place",
+        filled: true,
+        fillColor: Colors.grey.shade100,
+        contentPadding: const EdgeInsets.symmetric(vertical: 14),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
